1e8938716b0bcbfea23f368cac53564c09058cd2
Jorge Cabrera Sánchez <jorge.cabrera.sanchez1@gmail.com>
2017-03-02 17:13:35 +0100

    02/03/2017 -- 17:13 Calculadora2 terminada

--- src/calculadora2/FXMLDocumentController.java
+++ src/calculadora2/FXMLDocumentController.java
@@ -14,15 +14,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 /**
  *
  * @author daw
  */
 public class FXMLDocumentController implements Initializable {
-    
-    @FXML
-    private Label label;
+
+    String hace = "";
+    double num1 = 0, num2 = 0;
+
     @FXML
     private TextField resultado;
     @FXML
@@ -57,57 +59,104 @@ public class FXMLDocumentController implements Initializable {
     private Button limpiar;
     @FXML
     private Button resta;
-    
+    @FXML
+    private Text historico;
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        if (event.getSource() == cero){
+
+        if (event.getSource() == cero) {
             this.resultado.appendText("0");
-        } else if (event.getSource() == uno){
+        } else if (event.getSource() == uno) {
             this.resultado.appendText("1");
-        } else if (event.getSource() == dos){
+        } else if (event.getSource() == dos) {
             this.resultado.appendText("2");
-        } else if (event.getSource() == tres){
+        } else if (event.getSource() == tres) {
             this.resultado.appendText("3");
-        } else if (event.getSource() == cuatro){
+        } else if (event.getSource() == cuatro) {
             this.resultado.appendText("4");
-        } else if (event.getSource() == cinco){
+        } else if (event.getSource() == cinco) {
             this.resultado.appendText("5");
-        } else if (event.getSource() == seis){
+        } else if (event.getSource() == seis) {
             this.resultado.appendText("6");
-        } else if (event.getSource() == siete){
+        } else if (event.getSource() == siete) {
             this.resultado.appendText("7");
-        } else if (event.getSource() == ocho){
+        } else if (event.getSource() == ocho) {
             this.resultado.appendText("8");
-        } else if (event.getSource() == nueve){
+        } else if (event.getSource() == nueve) {
             this.resultado.appendText("9");
+
+        } else if (event.getSource() == suma) {
+            antes();
+            this.historico.setText(this.historico.getText() + this.resultado.getText() + " + ");
+            hace = "sumar";
+            this.resultado.setText("");
+
+        } else if (event.getSource() == resta) {
+            antes();
+            this.historico.setText(this.historico.getText() + this.resultado.getText() + " - ");
+            hace = "restar";
+            this.resultado.setText("");
+
+        } else if (event.getSource() == multiplicacion) {
+            antes();
+            this.historico.setText(this.historico.getText() + this.resultado.getText() + " * ");
+            hace = "multiplicar";
+            this.resultado.setText("");
+
+        } else if (event.getSource() == division) {
+            antes();
+            this.historico.setText(this.historico.getText() + this.resultado.getText() + " / ");
+            hace = "dividir";
+            this.resultado.setText("");
+
+        } else if (event.getSource() == igual) {
+            this.historico.setText("");
+            antes();
+            this.resultado.setText(String.valueOf(num1));
+            hace = "";
+            num1 = 0;
             
-        } else if (event.getSource() == suma){
-            double dato = Double.parseDouble(this.resultado.getText());
-            this.resultado.appendText(" + ");
-        } else if (event.getSource() == resta){
-            double dato = Double.parseDouble(this.resultado.getText());
-            this.resultado.appendText(" - ");
-        } else if (event.getSource() == multiplicacion){
-            double dato = Double.parseDouble(this.resultado.getText());
-            this.resultado.appendText(" * ");
-        } else if (event.getSource() == division){
-            double dato = Double.parseDouble(this.resultado.getText());
-            this.resultado.appendText(" / ");
-            
-        } else if (event.getSource() == igual){
-            StringTokenizer s = new StringTokenizer( this.resultado.getText(), "+/-*" ); 
-            while( s.hasMoreTokens() == true ){
-             //si hay mas trozos System.out.println( s.nextToken() );
-            }
-            double dato2 = Double.parseDouble();
-            
+
+        } else if (event.getSource() == limpiar) {
+            this.resultado.setText("");
         }
-        
+
     }
-    
+
+    private void antes() {
+        if (hace.isEmpty()) {
+            num1 = Double.parseDouble(this.resultado.getText());
+        } else {
+            num1 = operacion(hace, num1);
+        }
+    }
+
+    private double operacion(String operacion, double num1) {
+        double resultado = 0;
+        num2 = Double.parseDouble(this.resultado.getText());
+        switch (operacion) {
+            case "sumar":
+                resultado = num1 + num2;
+                break;
+            case "restar":
+                resultado = num1 - num2;
+                break;
+            case "multiplicar":
+                resultado = num1 * num2;
+                break;
+            case "dividir":
+                resultado = num1 / num2;
+                break;
+            default:
+                break;
+        }
+        return resultado;
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
